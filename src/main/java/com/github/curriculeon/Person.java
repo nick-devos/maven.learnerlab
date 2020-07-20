010b6159cd7e1e4913954ec885604c56d168ae20
nick-devos <nickdevos123@yahoo.com>
2020-07-20 19:59:01 -0400

    part 1

--- src/main/java/com/github/curriculeon/Person.java
+++ src/main/java/com/github/curriculeon/Person.java
@@ -1,5 +1,23 @@
 package com.github.curriculeon;
 
 public class Person {
+    private String name;
+    private final Long id;
 
+    public Person(Long id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getId() {
+        return id;
+    }
 }
